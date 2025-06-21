81c1f8543235ee01c3f4cb0607219d8d245c358b
Daniel de Lima Leal <daniellima.leal10@gmail.com>
2025-06-21 11:49:01 -0300

    fix(backend): add specific CORS origin for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'https://conectar-user-manager.vercel.app', // A URL do seu site na Vercel
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+
+  const config = new DocumentBuilder()
+    .setTitle('API Conectar - Gerenciamento de Usuários')
+    .setDescription('Documentação detalhada da API REST para o desafio Full-Stack.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
